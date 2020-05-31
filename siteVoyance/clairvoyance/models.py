fd4091bfb8f6909d1574086f09b0c0d109187e8d
NunoMars <nuno.ricardo.mars@gmail.com>
2020-05-31 23:54:02 +0200

    suppression migrations

--- siteVoyance/clairvoyance/models.py
+++ siteVoyance/clairvoyance/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class MajorArcana(models.Model):
     """ Class to define the mayor cards deck."""
-    card_name_fr = models.CharField(max_length=50)
-    card_signification_fr = models.TextField()
-    card_image = models.URLField()
+    card_name_pt = models.CharField(max_length=50)
+    card_signification_gen = models.TextField()
+    card_signification_warnings = models.TextField()
+    card_signification_love = models.TextField()
+    card_signification_work = models.TextField()
+    card_image = models.CharField(max_length=100)
