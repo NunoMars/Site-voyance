1fb38080983ba1578c050a9826aa3fac043afedd
NunoMars <nuno.ricardo.mars@gmail.com>
2020-06-02 15:24:14 +0200

    multilingue option

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    args= {}
+    page_title = _("Bienvenu dans mon Monde")
+    args["first_title"] = page_title
+    second_title = _("TAROT T")
+    args["second_title"] = second_title
+    return render(request, 'home/index.html', args)
