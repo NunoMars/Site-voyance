5d8a475597970bed41eaa5c152cd9ca5e7d4a5f4
NunoMars <nuno.ricardo.mars@gmail.com>
2020-06-15 23:27:42 +0200

    work on loops and do a menu response

--- clairvoyance/card_prints.py
+++ clairvoyance/card_prints.py
@@ -2,7 +2,6 @@ from random import shuffle as suf, randint as rand
 from datetime import datetime
 from .models import MajorArcana
 
-card_deck = [i+1 for i in range(38)]
 
 def one_card(input_value):
     rand_card = rand(0, 37)
@@ -33,55 +32,117 @@ def one_card(input_value):
         "</div></div> "}
 
 def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme):
-
+    card_deck = [i+1 for i in range(38)]
     suf(card_deck)
     column = len(name)
 
+    #construire tableau
+    def splitBy(li, n=1):
+        return [li[i:i+n] for i in range(0, len(li), n)]
     if deck_chosed == "left":
-        chosen_deck = card_deck[0:cut_point]  
+        chosen_deck = card_deck[0:cut_point]
+        print(chosen_deck)  
 
     if deck_chosed == "right":
         chosen_deck = card_deck[cut_point:37]
-
-    if chosed_theme == 'love':
-        chosed_theme = obj.card_signification_love
+        print(chosen_deck)
     
+    if chosed_theme == 'love':     
+
+        list_of_cards = []
+        for i in chosen_deck:        
+            obj = MajorArcana.objects.get(pk=i)
+            card_img = obj.card_image
+            card_name = obj.card_name_pt
+            chosed_theme = obj.card_signification_love
+            card_warnings = obj.card_signification_warnings
+            list_of_cards.append(
+                "<div class='col'><div class='cta-inner text-center rounded'>" +
+                "<h4><div class='mb-0'>" + card_name.capitalize() + "</div></h4>" +
+                "<p class='mb-0'><a href= '#'><img src= " + card_img +
+                " alt='card' height='25%' width='25%' /><span>" + card_warnings +
+                "</span></a></p>" +
+                "<div class='mb-0'><h4>" + "No trabalho" + "</h4></div>" +
+                "<p class='mb-0'>" + chosed_theme + "</p>" +
+                "</div></div> "
+            )
+
+        card_board = splitBy(list_of_cards, column)
+        final_card_deck = []
+        for i in card_board:
+            l = ''.join(i)
+            final_card_deck.append("<div class='row' height= '100%' text-align='center'>" + l + "</div>")
+        f = ''.join(final_card_deck)
+        print(f)
+        return {"messages" : "<div class='container'>" +
+        "<div class='col'><div class='cta-inner text-center rounded'>" + 
+        "<h4>" + name.capitalize() + " aqui esta o resultado das cartas</h4>" +
+        "<h6>Para saber os avisos do Tarot passe o rato en cima de cada carta!</h6>" +
+        f + "</div></div>"}
+
     if chosed_theme == 'work':
-        chosed_theme = obj.card_signification_work
-    
+
+        list_of_cards = []
+        for i in chosen_deck:        
+            obj = MajorArcana.objects.get(pk=i)
+            card_img = obj.card_image
+            card_name = obj.card_name_pt
+            chosed_theme = obj.card_signification_work
+            card_warnings = obj.card_signification_warnings
+            list_of_cards.append(
+                "<div class='col'><div class='cta-inner text-center rounded'>" +
+                "<h4><div class='mb-0'>" + card_name.capitalize() + "</div></h4>" +
+                "<p class='mb-0'><a href= '#'><img src= " + card_img +
+                " alt='card' height='25%' width='25%' /><span>" + card_warnings +
+                "</span></a></p>" +
+                "<div class='mb-0'><h4>" + "No trabalho" + "</h4></div>" +
+                "<p class='mb-0'>" + chosed_theme + "</p>" +
+                "</div></div> "
+            )
+
+        card_board = splitBy(list_of_cards, column)
+        final_card_deck = []
+        for i in card_board:
+            l = ''.join(i)
+            final_card_deck.append("<div class='row' height= '100%' text-align='center'>" + l + "</div>")
+        f = ''.join(final_card_deck)
+        print(f)
+        return {"messages" : "<div class='container'>" +
+        "<div class='col'><div class='cta-inner text-center rounded'>" + 
+        "<h4>" + name.capitalize() + " aqui esta o resultado das cartas</h4>" +
+        "<h6>Para saber os avisos do Tarot passe o rato en cima de cada carta!</h6>" +
+        f + "</div></div>"}
+            
     if chosed_theme == 'gen':
-        chosed_theme = obj.card_signification_gen
+        
+        list_of_cards = []
+        for i in chosen_deck:        
+            obj = MajorArcana.objects.get(pk=i)
+            card_img = obj.card_image
+            card_name = obj.card_name_pt
+            chosed_theme = obj.card_signification_gen
+            card_warnings = obj.card_signification_warnings
+            list_of_cards.append(
+                "<div class='col'><div class='cta-inner text-center rounded'>" +
+                "<h4><div class='mb-0'>" + card_name.capitalize() + "</div></h4>" +
+                "<p class='mb-0'><a href= '#'><img src= " + card_img +
+                " alt='card' height='25%' width='25%' /><span>" + card_warnings +
+                "</span></a></p>" +
+                "<div class='mb-0'><h4>" + "No trabalho" + "</h4></div>" +
+                "<p class='mb-0'>" + chosed_theme + "</p>" +
+                "</div></div> "
+            )
 
-    #construire tableau
-    def splitBy(li, n=1):
-        return [li[i:i+n] for i in range(0, len(li), n)] 
-
-    list_of_cards = []
-    for i in chosen_deck:        
-        obj = MajorArcana.objects.get(pk=i)
-        card_img = obj.card_image
-        card_name = obj.card_name_pt
-        card_warnings = obj.card_signification_warnings
-        print(card_warnings)
-        list_of_cards.append(
-            "<div class='col'><div class='cta-inner text-center rounded'>" +
-            "<h6><div class='mb-0'>" + card_name.capitalize() + "</div></h6>" +
-            "<p class='mb-0'><a href= '#'><img src= " + card_img +
-            " alt='card' height='25%' width='25%' /><span>" + card_warnings +
-            "</span></a></p>" +
-            "<div class='mb-0'><h4>" + "No trabalho" + "</h4></div>" +
-            "<p class='mb-0'>" + chosed_theme + "</p>" +
-            "</div></div> "
-        )
-
-    card_board = splitBy(list_of_cards, column)
-    final_card_deck = []
-    for i in card_board:
-        l = ''.join(i)
-        final_card_deck.append("<div class='row' height= '100%' text-align='center'>" + l + "</div>")
-    f = ''.join(final_card_deck)
-    print(f)
-    return {"messages" : "<div class='container'>" + 
-    "<h4>Aqui esta o resultado das cartas</h4>" +
-    "<h6>Para saber os avisos do Tarot passe o rato en cima de cada carta!</h6>" +
-    f + "</div>"}
+        card_board = splitBy(list_of_cards, column)
+        final_card_deck = []
+        for i in card_board:
+            l = ''.join(i)
+            final_card_deck.append("<div class='row' height= '100%' text-align='center'>" + l + "</div>")
+        f = ''.join(final_card_deck)
+        print(f)
+        return {"messages" : "<div class='container'>" +
+        "<div class='col'><div class='cta-inner text-center rounded'>" + 
+        "<h4>" + name.capitalize() + " aqui esta o resultado das cartas</h4>" +
+        "<h6>Para saber os avisos do Tarot passe o rato en cima de cada carta!</h6>" +
+        f + "</div></div>"}
+        
--- clairvoyance/logic.py
+++ clairvoyance/logic.py
@@ -4,82 +4,87 @@ from .card_prints import one_card, clairvoyante_sort_cards
 #faire Connaiscance
 inputs = []
 def clairvoyant(input_value):
-    #création deck
-    card_deck = [i+1 for i in range(38)]
+    while True:        
+        #création deck
+        card_deck = [i+1 for i in range(38)]    
+        
+        if input_value not in inputs:
+            inputs.append(input_value)
+            print(inputs)
 
-    
-    if input_value not in inputs:
-        inputs.append(input_value)
-        print(inputs)
-
-    """while True:
-        del inputs[1:]"""
-
-    if (len(inputs) == 1):        
-
-        msg = {"messages" : "<div class='col'><div class='cta-inner text-center rounded'>" +
-        "<p class='mb-1'>Muito obrigada " + input_value.capitalize() + " !" +
-        " Vou baralhando as cartas..." +
-        "<div class='container'><div class='cta-inner text-center rounded'>" +
-        "<div class='col' height= '100%' text-align='center'>" +
-        "<p class=class='mb-0'>Escolha o tema da pergunta!</p>" +
-        "<p class=class='mb-0'>Clique no baralho para escolher o baralho</p></div>" +
-        "<div class='row' height= '100%' text-align='center'>" +
-        "<p class=class='mb-0'><h6>" + "Pergunta sobre AMOR" + "<h6></p>"
-        "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageLove();'/></div></p>" +
-        "<p class=class='mb-0'><h6>" + "Pergunta sobre TRABALHO" + "</h6></p>"
-        "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageWork();'/></div></p>" +
-        "<p class=class='mb-0'><h6>" + "Pergunta sobre SITUACAO EN GERAL" + "</h6></p>"
-        "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageGen();'/></div></p>" +
-        "<p class=class='mb-0'><h6>" + "Pergunta RAPIDA (una carta)" + "</h6></p>"
-        "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageOneCard();'/></div><p/>" +
-        "</div></div>"
-        } 
-
-        return msg
-
-    if inputs[1] == "one":
         input_name = inputs[0]
-        value = one_card(input_name)
-        return value
-        del inputs[1:]
-    else:
-        if (len(inputs) == 2):
+        
+        if (len(inputs) == 1):        
 
-            return {"messages" : "<div class='col'><div class='cta-inner text-center rounded'>" +
-            "<p class='mb-1'>Obrigada !</p>" +
-            " <p class='mb-1'>Estamos quase a saber o que o Tarot nos diz!</p>" +
-            " <p class='mb-1'>Clique no baralho para cortar en dois</p>" +
-            "<p class='mb-1'><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageCut();'/></p>" +
-            "</div>"
-            }
-
-        if inputs[2] == "cut":
-            cut_point = rand(0, 37)
-
-        if (len(inputs) == 3):
-            
-            return {"messages" : "<div class='container'><div class='cta-inner text-center rounded'>" +
+            msg = {"messages" : "<div class='col'><div class='cta-inner text-center rounded'>" +
+            "<p class='mb-1'>Muito obrigada " + input_value.capitalize() + " !" +
+            " Vou baralhando as cartas..." +
+            "<div class='container'><div class='cta-inner text-center rounded'>" +
+            "<div class='col' height= '100%' text-align='center'>" +
+            "<p class=class='mb-0'>Escolha o tema da pergunta!</p>" +
+            "<p class=class='mb-0'>Clique no baralho para escolher o baralho</p></div>" +
             "<div class='row' height= '100%' text-align='center'>" +
-            "<p class='mb-1'>Obrigada !</p>" +
-            " <p class='mb-1'>Temos aqui os dois baralhos!</p>" +
-            " <p class='mb-1'>Clique no baralho para escolher o baralho</p></div>" +
-            "<div class='row' height= '100%' text-align='center'>" +
-            "<div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageLeft();'/></div>" +
-            "<div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageRight();'/></div>" +
+            "<p class=class='mb-0'><h6>" + "Pergunta sobre AMOR" + "<h6></p>"
+            "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageLove();'/></div></p>" +
+            "<p class=class='mb-0'><h6>" + "Pergunta sobre TRABALHO" + "</h6></p>"
+            "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageWork();'/></div></p>" +
+            "<p class=class='mb-0'><h6>" + "Pergunta sobre SITUACAO EN GERAL" + "</h6></p>"
+            "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageGen();'/></div></p>" +
+            "<p class=class='mb-0'><h6>" + "Pergunta RAPIDA (una carta)" + "</h6></p>"
+            "<p><div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageOneCard();'/></div><p/>" +
             "</div></div>"
-            }
+            } 
 
-        deck_chosed = inputs[3]       
+            return msg
 
-        if inputs[1] == "love":
-            result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"love")
-            return result
-        if inputs[1] == "work":
-            result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"work")
-            return result
-        if inputs[1] == "gen":
-            result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"gen")
-            return result
+        while True:
 
+            if inputs[1] == "one":
+                input_name = inputs[0]
+                value = one_card(input_name)
+                return value
+
+            else:
+                if (len(inputs) == 2):
+
+                    return {"messages" : "<div class='col'><div class='cta-inner text-center rounded'>" +
+                    "<p class='mb-1'>Obrigada !</p>" +
+                    " <p class='mb-1'>Estamos quase a saber o que o Tarot nos diz!</p>" +
+                    " <p class='mb-1'>Clique no baralho para cortar en dois</p>" +
+                    "<p class='mb-1'><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageCut();'/></p>" +
+                    "</div>"
+                    }
+
+                if inputs[2] == "cut":
+                    cut_point = rand(0, 37)
+
+                if (len(inputs) == 3):
+                    
+                    return {"messages" : "<div class='container'><div class='cta-inner text-center rounded'>" +
+                    "<div class='row' height= '100%' text-align='center'>" +
+                    "<p class='mb-1'>Obrigada !</p>" +
+                    " <p class='mb-1'>Temos aqui os dois baralhos!</p>" +
+                    " <p class='mb-1'>Clique no baralho para escolher o baralho</p></div>" +
+                    "<div class='row' height= '100%' text-align='center'>" +
+                    "<div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageLeft();'/></div>" +
+                    "<div class='col''><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageRight();'/></div>" +
+                    "</div></div>"
+                    }
+
+                deck_chosed = inputs[3]       
+
+                if inputs[1] == "love":                    
+                    result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"love")
+                    return result                 
+
+                if inputs[1] == "work":
+                    result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"work")
+                    return result
+
+                if inputs[1] == "gen":
+                    result = clairvoyante_sort_cards(input_name, cut_point, deck_chosed,"gen")
+                    return result
+            continue
+        del inputs[1:]
+        continue
 
