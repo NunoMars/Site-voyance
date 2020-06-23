531926bfc6fd3e13dfa852fe80ecfe7137cb3979
NunoMars <nuno.ricardo.mars@gmail.com>
2020-06-24 01:06:29 +0200

    clairvoyant over

--- clairvoyance/card_prints.py
+++ clairvoyance/card_prints.py
@@ -3,37 +3,71 @@ from django.utils.translation import gettext as _
 from datetime import datetime
 from .models import MajorArcana
 
-
-
-def one_card(input_value, rand_card, menu):
-    obj = MajorArcana.objects.get(pk=rand_card)
+def get_from_db(id_card):
+    obj = MajorArcana.objects.get(pk=id_card)
     card_img = obj.card_image
     card_name = obj.card_name_pt
     card_warnings = obj.card_signification_warnings
     card_signification_gen = obj.card_signification_gen
     card_signification_love = obj.card_signification_love
     card_signification_work= obj.card_signification_work
-    name= input_value
+    card_polarity = obj.card_polarity
+    return [
+        card_img,
+        card_name,
+        card_warnings,
+        card_signification_gen,
+        card_signification_love,
+        card_signification_work,
+        card_polarity
+        ]
+
+def one_card(name, rand_card, menu):
+    card = get_from_db(rand_card)
 
     return {"messages" : "<div class='container'><div class='cta-inner text-center rounded'>" +
         "<div class='col'><div class='cta-inner text-center rounded'>" +
         "<div class='mb-0'><h2>" + name.capitalize() + _(" o que o tarot tem para lhe dizer!") + "</h2></div>" +        
         "<div class='mb-0'><a href='#'><img src='/static/img/cards/Back.jpg'" +
-        "onmouseover=" + '"this.src=' + "'" + card_img + "'" + '"' +
+        "onmouseover=" + '"this.src=' + "'" + card[0] + "'" + '"' +
         "border='0' alt='' height='25%' width='25%'/></a></div>" +
-        "<div class='mb-0'><h3>" + _(card_name.capitalize()) + "</h3></div>" +          
-        "<div class='mb-0'><h4>" + _("Significado en geral") + "</h4></div>" +
-        "<p class='mb-0'>" + _(card_signification_gen) + "</p>" +
+        "<div class='mb-0'><h3>" + _(card[1].capitalize()) + "</h3></div>" +
         "<div class='mb-0'><h3>" + _("Atenção") + "</h3></div>" +
-        "<p class='mb-0'>" + _(card_warnings) + "</p>" +
+        "<p class='mb-0'>" + _(card[2]) + "</p>" +          
+        "<div class='mb-0'><h4>" + _("Significado en geral") + "</h4></div>" +
+        "<p class='mb-0'>" + _(card[3]) + "</p>" +        
         "<div class='mb-0'><h4>" + _("No amor") + "</h4></div>" +
-        "<p class='mb-0'>" + _(card_signification_love) + "</p>" +
+        "<p class='mb-0'>" + _(card[4]) + "</p>" +
         "<div class='mb-0'><h4>" + _("No trabalho") + "</h4></div>" +
-        "<p class='mb-0'>" + _(card_signification_work) + "</p>" + 
+        "<p class='mb-0'>" + _(card[5]) + "</p>" + 
         "</div></div></div></div> " + menu["messages"]
         }
 
-def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme, menu):
+def response_card(name, index_result_card, chosed_theme, menu):
+    card = get_from_db(index_result_card)
+    if chosed_theme == "love":
+        chosed_theme = card[4]
+    if chosed_theme == "work":
+        chosed_theme = card[5]
+    if chosed_theme == "gen":
+        chosed_theme = card[3]
+
+    return {"messages" : "<div class='container'><div class='cta-inner text-center rounded'>" +
+        "<div class='col'><div class='cta-inner text-center rounded'>" +
+        "<div class='mb-0'><h2>" + name.capitalize() + _(" o que o tarot tem para lhe dizer!") + "</h2></div>" +        
+        "<div class='mb-0'><a href='#'><img src='/static/img/cards/Back.jpg'" +
+        "onmouseover=" + '"this.src=' + "'" + card[0] + "'" + '"' +
+        "border='0' alt='' height='25%' width='25%'/></a></div>" +
+        "<div class='mb-0'><h3>" + _(card[1].capitalize()) + "</h3></div>" +
+        "<div class='mb-0'><h4>" + _("Resposta do tarot") + "</h4></div>" +
+        "<p class='mb-0'>" + _(chosed_theme) + "</p>" + 
+        "<div class='mb-0'><h3>" + _("Atenção") + "</h3></div>" +
+        "<p class='mb-0'>" + _(card[2]) + "</p>" +          
+        "</div></div></div></div> " + menu["messages"]
+        }
+
+
+def clairvoyante_sort_cards(name, chosed_card_deck, chosed_theme, menu):
    
     card_deck = [i+1 for i in range(38)]
     suf(card_deck)
@@ -42,34 +76,29 @@ def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme, menu):
     #construire tableau
     def splitBy(li, n=1):
         return [li[i:i+n] for i in range(0, len(li), n)]
-    #chisir le tas pour afficher
-    if deck_chosed == "left":
-        chosen_deck = card_deck[0:cut_point]
-
-    if deck_chosed == "right":
-        chosen_deck = card_deck[cut_point:37]
-    
+ 
 
     def create_cards_message(card_name, card_img, card_warnings, chosed_theme):
         msg =["<div class='col'>" +
-        "<div class='cta-inner text-center rounded'>" +
-        "<p class='mb-0'><h4>" + _(card_name.capitalize()) + "</h4></p>" +
-        "<p class='mb-0'><a href='#'><img class='card' src='/static/img/cards/Back.jpg'" +
+        "<div class='cta-inner text-center rounded'>" +               
+        "<a href='#'><img class='card' src='/static/img/cards/Back.jpg'" +
         "onmouseover=" + '"this.src=' + "'" + card_img + "'" + '"' +
         "onmouseout=" + '"this.src=' + "'/static/img/cards/Back.jpg'" + '"' +
-        "border='0' alt=''/>" +          
-        "<span>" + _(card_warnings) + _(chosed_theme) +
-        "</span></a></p>" +                       
+        "border='0' alt=''/>" +                 
+        "<span><p>" + _(card_name.capitalize()) + "</p>" +
+        "<p>" + _("Atenção") + "</p>" +
+        (card_warnings) +
+        "<p>" + _("A mensagen da carta!") + "</p>" +
+        _(chosed_theme) +
+        "</span></a>" +                       
         "</div></div>"]
         return msg
 
-    def create_final_response(list_of_cards, menu, name, list_of_polarity, chosen_deck):
-        
+    def create_final_response(list_of_cards, menu, name, list_of_polarity, chosed_card_deck):      
 
 
         def polarity_calcul(list_of_polarity):
 
-            print(list_of_polarity)
 
             items_on_list = len(list_of_polarity)
 
@@ -104,13 +133,13 @@ def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme, menu):
 
             if percentage_positif > percentage_negatif:
                 msg = [
-                    _(" O resultado é positivo com ") + str(percentage_positif) + _("% de cartas a favor!")
+                    _(" O resultado é positivo com ") + str(percentage_positif) + _("% total de cartas a favor!")
                 ]
                 return msg[0]
             if percentage_negatif > percentage_positif:
                 msg = [
                     _(" O resultado é negativo com ") + str(percentage_negatif) +
-                     _("% de cartas en desfavor, mâs, tudo tem solução o Tarot vai-lhe indicar o caminho...!")
+                     _("% total de cartas en desfavor, mâs, tudo tem solução o Tarot vai-lhe indicar o caminho...!")
                 ]
                 return msg[0]
             if percentage_neutral > percentage_positif or percentage_neutral > percentage_negatif:
@@ -121,11 +150,11 @@ def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme, menu):
                 return msg[0]
 
         
-        def average(chosen_deck):
-            return sum(chosen_deck)/len(chosen_deck)
+        def average(chosed_card_deck):
+            return sum(chosed_card_deck)/len(chosed_card_deck)
         
-        index_result_card = round(average(chosen_deck))
-        result = one_card(name, index_result_card, menu)
+        index_result_card = round(average(chosed_card_deck))
+        important_card = response_card(name, index_result_card, chosed_theme, menu)
         card_board = splitBy(list_of_cards, column)
         final_card_deck = []
         for i in card_board:
@@ -141,59 +170,41 @@ def clairvoyante_sort_cards(name, cut_point, deck_chosed, chosed_theme, menu):
         "<h4>" + name.capitalize() + _(" aqui esta o resultado das cartas") + "</h4>" +
         "<h4>" + polarity + "</h4>" +
         "<h6>" + _("Para saber os avisos do Tarot passe o rato en cima de cada carta!") + "</h6>" +
-        f + "</div></div>" + result["messages"]
+        f + "</div></div>" + important_card["messages"]
         }    
     
     if chosed_theme == 'love':     
 
         list_of_cards = []
         list_of_polarity = []
-        for i in chosen_deck:        
-            obj = MajorArcana.objects.get(pk=i)
-            card_img = obj.card_image
-            card_name = obj.card_name_pt
-            chosed_theme = obj.card_signification_love
-            card_warnings = obj.card_signification_warnings
-            card_polarity = obj.card_polarity
-            list_of_polarity.append(card_polarity)
-            message_card = create_cards_message(card_name, card_img, card_warnings, chosed_theme)
+        for i in chosed_card_deck:
+            card = get_from_db(i)        
+            list_of_polarity.append(card[6])
+            message_card = create_cards_message(card[1], card[0], card[2], card[4])
             list_of_cards.append(message_card[0])
-
-        return create_final_response(list_of_cards, menu, name, list_of_polarity, chosen_deck)
 
     if chosed_theme == 'work':
 
         list_of_cards = []
         list_of_polarity = []
-        for i in chosen_deck:        
-            obj = MajorArcana.objects.get(pk=i)
-            card_img = obj.card_image
-            card_name = obj.card_name_pt
-            chosed_theme = obj.card_signification_work
-            card_warnings = obj.card_signification_warnings
-            card_polarity = obj.card_polarity
-            list_of_polarity.append(card_polarity)
-            message_card = create_cards_message(card_name, card_img, card_warnings, chosed_theme)
+        for i in chosed_card_deck:        
+            card = get_from_db(i)        
+            list_of_polarity.append(card[6])
+            message_card = create_cards_message(card[1], card[0], card[2], card[5])
             list_of_cards.append(message_card[0])
 
 
-        return create_final_response(list_of_cards, menu, name, list_of_polarity, chosen_deck)
             
     if chosed_theme == 'gen':
         
         list_of_cards = []
         list_of_polarity = []
-        for i in chosen_deck:        
-            obj = MajorArcana.objects.get(pk=i)
-            card_img = obj.card_image
-            card_name = obj.card_name_pt
-            chosed_theme = obj.card_signification_gen
-            card_warnings = obj.card_signification_warnings
-            card_polarity = obj.card_polarity
-            list_of_polarity.append(card_polarity)
-            message_card = create_cards_message(card_name, card_img, card_warnings, chosed_theme)
+        for i in chosed_card_deck:        
+            card = get_from_db(i)        
+            list_of_polarity.append(card[6])
+            message_card = create_cards_message(card[1], card[0], card[2], card[3])
             list_of_cards.append(message_card[0])
 
 
-        return create_final_response(list_of_cards, menu, name, list_of_polarity, chosen_deck)
+    return create_final_response(list_of_cards, menu, name, list_of_polarity, chosed_card_deck)
         
--- clairvoyance/logic.py
+++ clairvoyance/logic.py
@@ -5,20 +5,19 @@ from .card_prints import one_card, clairvoyante_sort_cards
 #faire Connaiscance
 inputs = []
 def clairvoyant(input_value):
-        
+    card_deck = [i+1 for i in range(38)] 
+
     if input_value not in inputs:
         inputs.append(input_value)
-
-    
+          
+  
     while True:
         #création deck
-        card_deck = [i+1 for i in range(38)]    
-        input_name = inputs[0]
 
         menu = {"messages" : "<div class='container' width = '100%'><div class='cta-inner text-center rounded'>" +
             "<div class='row'>" +
             "<div class='col'>" +
-            "<p><h6 class='mb-0'>" + _("Muito obrigada ")  + input_name.capitalize() + " !</h6></p>" +
+            "<p><h6 class='mb-0'>" + _("Muito obrigada ")  + inputs[0].capitalize() + " !</h6></p>" +
             "<p><h5 class='mb-0'>" + _(" Vou baralhando as cartas...") + "</h5></p></div></div>" +            
             "<div class='row'>" +
             "<div class='col'>" +
@@ -40,18 +39,28 @@ def clairvoyant(input_value):
             "<p><div class='mb-0'><input id='bouton_card' type='submit' class='bouton_card' onClick='sendMessageOneCard();'/></div><p/>" +
             "</div></div></div></div>"
             } 
-
         if (len(inputs) == 1):
-            return menu        
+            return menu     
         
         if input_value == "one":
             card_deck = [i+1 for i in range(38)]
             suf(card_deck)
             rand_card = choice(card_deck)
-            value = one_card(input_name, rand_card, menu)
+            value = one_card(inputs[0], rand_card, menu)
             del inputs[1:]
             return value
 
+        
+        if input_value == "love":
+            inputs[1] = "love"
+       
+
+        if input_value == "work":
+            inputs[1] = "work"
+
+
+        if input_value == "gen":
+            inputs[1] = "gen"
 
         if (len(inputs) == 2):
             input_name = inputs[0]
@@ -79,25 +88,16 @@ def clairvoyant(input_value):
             "</div>"
             }
 
-        if inputs[1] == "love":
-            print(inputs)                    
-            result = clairvoyante_sort_cards(inputs[0], inputs[2], inputs[3], inputs[1], menu)
-            del inputs[1:]
-            print(inputs)
-            return result                 
+                #chisir le tas pour afficher
+        if input_value == "left":
+            inputs[3] = card_deck[0:inputs[2]]
 
-        if inputs[1] == "work":
-            result = clairvoyante_sort_cards(inputs[0], inputs[2], inputs[3], inputs[1], menu)
-            del inputs[1:]
+        if input_value == "right":
+            inputs[3] = card_deck[inputs[2]:37]
 
-            return result
 
-        if inputs[1] == "gen":
-            result = clairvoyante_sort_cards(inputs[0], inputs[2], inputs[3], inputs[1], menu)
-            del inputs[1:]
-
-            return result
-
+        result = clairvoyante_sort_cards(inputs[0], inputs[3], inputs[1], menu)
         del inputs[1:]
-        continue
+        return result
+
 
