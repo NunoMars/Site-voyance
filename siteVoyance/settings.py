562998888abf2d57cf0376d228dbbd7f91f634f5
NunoMars <nuno.ricardo.mars@gmail.com>
2020-10-08 20:45:43 +0200

    hide secret key

--- siteVoyance/settings.py
+++ siteVoyance/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nj3!-cfwk8&)-mxx31agx^3!2ztqx(&g-0fx0e5qi2)no!ng-s'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "devlopment") == "production" else True
 
