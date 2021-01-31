8f088a252eaa1dc75aff7ef998a33f3a5c3d288a
nuno <nuno.ricardo.mars@gmail.com>
2021-01-31 15:47:19 +0100

    try deploiement

--- siteVoyanceconfig/settings.py
+++ siteVoyanceconfig/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get("ENV", "production") == "production" else True
+DEBUG = False if os.environ.get("ENV") == "production" else True
 
 ALLOWED_HOSTS = ["herokuapps.com", "localhost", "127.0.0.1"]
 
