ce2096eca82493065f6ecebd82f278ea344d04a0
nuno <nuno.ricardo.mars@gmail.com>
2021-02-09 09:22:52 +0100

    reorganisation html base

--- siteVoyanceconfig/settings/__init__.py
+++ siteVoyanceconfig/settings/__init__.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-pt'
+LANGUAGE_CODE = 'pt-PT'
 
 TIME_ZONE = 'UTC'
 
